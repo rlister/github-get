f06e1d1cd4ea4820adb45f0c361658e76356e794
Richard Lister <rlister+gh@gmail.com>
2015-02-18 00:28:32 -0500

    handle src ending in / as dir and get all files

--- github-get.go
+++ github-get.go
@@ -44,10 +44,15 @@ func client(repo string, token string) func(string) []byte {
 }
 
 func main() {
+
+	// function to get content from github
 	get := client(os.Getenv("REPO"), os.Getenv("TOKEN"))
 
 	// loop arguments
 	for _, arg := range os.Args[1:] {
+
+		// map of filename to content
+		output := make(map[string][]byte)
 
 		// optionally request output file with src:dest
 		files := strings.SplitN(arg, ":", 2)
@@ -57,20 +62,45 @@ func main() {
 		if strings.HasSuffix(src, "/") {
 			dirname := get(strings.TrimSuffix(src, "/"))
 
-		// write to requested file, or stdout
-		if len(files) > 1 {
-			// mkpath
-			err := os.MkdirAll(path.Dir(files[1]), 0755)
 			// parse json
 			var dir []interface{}
 			err := json.Unmarshal(dirname, &dir)
 			check(err)
 
-			// write the file
-			err = ioutil.WriteFile(files[1], output, 0644)
+			// add each file to the output map
+			for _, item := range dir {
+				file := item.(map[string]interface{})
+				name := file["name"].(string)
+				if file["type"].(string) == "file" {
+					output[name] = get(path.Join(src, name))
+				}
+			}
+
+		} else { // get src that is a single named file
+			output[path.Base(src)] = get(src)
+		}
+
+		if len(files) == 1 { // no destination given
+
+			// write each file to stdout
+			for _, content := range output {
+				fmt.Println(string(content))
+			}
+
+		} else { // write to given destination directory
+
+			dest := files[1]
+
+			// make output dir
+			err := os.MkdirAll(dest, 0755)
 			check(err)
-		} else {
-			fmt.Println(string(output))
+
+			// write each file to output dir
+			for file, content := range output {
+				err := ioutil.WriteFile(path.Join(dest, file), content, 0644)
+				check(err)
+			}
+
 		}
 	}
 }
